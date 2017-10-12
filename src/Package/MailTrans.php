b0e7adae8621f8a6d255282e63af03d896cfde0a
Tibor Hegedüs <contact@hegedustibor.hu>
2017-10-12 14:31:41 +0200

    [Add] attachment.
    You can add an attachment to your mail.

--- src/Package/MailTrans.php
+++ src/Package/MailTrans.php
@@ -13,15 +13,18 @@ class MailTrans
         $this->default_lang = $config['default_lang'];
     }
 
-    public function sendMail(Mail $mail, $data, $recipient_email, $recipient_name = "")
+    public function sendMail(Mail $mail, $data, $recipient_email, $recipient_name = "", $attachment = null)
     {
         $subject = $mail->compileSubject($data);
         $body = $mail->compileBody($data);
-        \Mail::send($mail->view_name, ['body' => $body], function ($message) use($mail, $subject, $recipient_email, $recipient_name) {
+        \Mail::send($mail->view_name, ['body' => $body], function ($message) use($mail, $subject, $recipient_email, $recipient_name, $attachment) {
 
             $message->from($mail->sender_email, $mail->sender_name);
 
             $message->subject($subject);
+            if ($attachment !== null) {
+                $message->attach($attachment);
+            }
 
             $message->to($recipient_email,$recipient_name);
         });
